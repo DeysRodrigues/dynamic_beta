13289989b4f22abd2eb2b8cb9e9d8baaa30b20db
Deys <deysprogrammer@gmail.com>
2025-07-04 23:42:58 -0300

    feat: add component page

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import Dashboard from"../components/layout/Dashboard";
+
+
+export default function Home() {
+  return (
+      <main className="flex-1 overflow-y-auto p-6 bg-gray-50">
+        <Dashboard />
+      </main>
+   
+  );
+}
