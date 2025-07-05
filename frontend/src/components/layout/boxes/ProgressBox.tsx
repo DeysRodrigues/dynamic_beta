4d5991d4611565a6cb1a36a7436aa3bcda4cc486
Deys <deysprogrammer@gmail.com>
2025-07-04 23:39:14 -0300

    feat: implement component logic

--- frontend/src/components/layout/boxes/ProgressBox.tsx
+++ frontend/src/components/layout/boxes/ProgressBox.tsx
@@ -1,14 +1,30 @@
+import { useTaskContext } from "@/context/TaskContext";
+
 interface ProgressBoxProps {
   className?: string;
 }
 
 export default function ProgressBox({ className }: ProgressBoxProps) {
+  
+  const { tasks } = useTaskContext();
+
+  const totalTasks = tasks.length;
+  const completedTasks = tasks.filter((task) => task.completed).length;
+  const progressPercent =
+    totalTasks === 0 ? 0 : Math.round((completedTasks / totalTasks) * 100);
+
   return (
     <div className={`box-padrao ${className}`}>
       <h2 className="text-lg font-semibold">Progresso nas tarefas</h2>
-      <p>3 de 12 tasks (20%)</p>
+      <p>
+        {" "}
+        {completedTasks} de {totalTasks} tasks ({progressPercent}%){" "}
+      </p>
       <div className="w-full bg-white h-3 rounded-full overflow-hidden">
-        <div className="h-full bg-gradient-to-r from-indigo-500 to-purple-500" style={{ width: "25%" }}></div>
+        <div
+          className="h-full bg-gradient-to-r from-indigo-500 to-purple-500"
+          style={{ width: `${progressPercent}%` }}
+        ></div>
       </div>
     </div>
   );
