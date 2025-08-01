728c97bee571c44742975ebe67965d2b8e6e8e68
Deys <deysprogrammer@gmail.com>
2025-08-01 20:54:39 -0300

    style: improve layout and typing for TaskItem component

--- frontend/src/components/ui/TaskItem.tsx
+++ frontend/src/components/ui/TaskItem.tsx
@@ -5,7 +5,7 @@ interface Task {
   time: string;
   description: string;
   tag?: string;
-  completed?: boolean;
+  completed: boolean | undefined;
 }
 
 interface TaskItemProps {
@@ -24,14 +24,14 @@ export default function TaskItem({
   editable = false,
 }: TaskItemProps) {
   return (
-    <div className="flex justify-between items-center gap-2 bg-slate-100 w-full p-3 rounded-full sm:flex-row sm:bg-white">
-      <div className="flex w-full sm:w-[60%] sm:gap-3">
+    <div className="flex justify-between items-center gap-2 bg-white-100 w-full p-3 rounded-full sm:flex-row sm:bg-white">
+      <div className="flex w-full sm:w-[90%] ">
         {/* Hora */}
         <input
           type="time"
           value={task.time}
           readOnly
-          className="text-gray-700 font-bold p-1 rounded-lg"
+          className="text-center text-gray-700 font-bold p-1 rounded-lg"
         />
 
         {/* Descrição */}
@@ -42,7 +42,7 @@ export default function TaskItem({
             editable && onChange?.(task.id, "description", e.target.value)
           }
           readOnly={!editable}
-          className={`text-gray-600 font-medium flex-1 rounded-lg px-3 py-1 text-center w-full ${
+          className={`text-gray-600 font-medium text-[0.9rem] flex-1 rounded-lg px-3 py-1 text-center w-full ${
             task.completed ? "bg-emerald-100" : "bg-white"
           }`}
         />
@@ -66,9 +66,18 @@ export default function TaskItem({
         {onToggle && (
           <div
             onClick={() => onToggle(task.id)}
-            className={`w-6 h-6 rounded-full cursor-pointer ${
-              task.completed ? "bg-primary" : "bg-gray-300"
-            }`}
+            className={`
+    w-6 h-6 rounded-full cursor-pointer border
+ ${
+   task.completed === undefined
+     ? "bg-gray-300 border-gray-400"
+     : task.completed === true
+     ? "bg-green-500 border-green-600"
+     : "bg-red-500 border-red-600"
+ }
+
+
+  `}
           ></div>
         )}
 
