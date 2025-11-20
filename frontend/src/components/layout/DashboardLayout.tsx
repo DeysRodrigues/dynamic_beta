ad5a6503e60b00be273e3dd795714cde4369efed
Deys <deysprogrammer@gmail.com>
2025-11-19 23:58:07 -0300

    chore: deleted out unused pages for now

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -8,9 +8,8 @@ export default function DashboardLayout() {
         items={[
           { label: "Home", path: "/" },
           { label: "Tasks", path: "/tasks" },
-          { label: "Timed Tasks", path: "/timedtasks" },
           { label: "Graphics", path: "/graphics" },
-          { label: "Configurações", path: "/settings" },
+      
         ]}
       />
       <main className="flex-1 overflow-y-auto bg-gray-50 sm:p-5">
