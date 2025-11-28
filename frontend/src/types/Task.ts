53b45c1b2c7b2350eb43ab6f598e88bf66182c4c
Deys <deysprogrammer@gmail.com>
2025-11-28 20:53:09 -0300

    refactor: updated Task type with groupTag field

--- frontend/src/types/Task.ts
+++ frontend/src/types/Task.ts
@@ -2,11 +2,13 @@
 
 export interface Task {
   id: string;
-  time: string;             // formato "HH:mm"
+  time?: string;             // formato "HH:mm"
   description: string;
   tag: string;
   completed: boolean | undefined;
 
   date: string;             // formato "YYYY-MM-DD"
   duration: number;         // duração em minutos
+
+  groupTag?: string;
 }
