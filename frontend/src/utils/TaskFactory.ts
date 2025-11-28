c5d813f8dd2456ddf59f5a7fb92e141bd439c699
Deys <deysprogrammer@gmail.com>
2025-11-28 20:57:23 -0300

    refactor(utils): updated TaskFactory to accept groupTag parameter

--- frontend/src/utils/TaskFactory.ts
+++ frontend/src/utils/TaskFactory.ts
@@ -1,30 +1,21 @@
 import type { Task } from "@/types/Task";
 import { getTodayDate } from "@/utils/DateUtils";
 
-
-/**
- * Cria uma nova tarefa padronizada com ID, data e estado de completude.
- * 
- * @param description - Texto da tarefa
- * @param time - Horário da tarefa no formato "HH:mm"
- * @param tag - Tag associada à tarefa
- * @param duration - Duração da tarefa em minutos
- * @returns Objeto `Task` formatado
- */
-
 export function createTask(
   description: string,
-  time: string,
+  time: string | undefined,
   tag: string,
-  duration: number
+  duration: number,
+  groupTag?: string // Novo parâmetro opcional
 ): Task {
   return {
-    id: Date.now().toString() + Math.random().toString(36), // Garante um ID único
+    id: Date.now().toString() + Math.random().toString(36),
     description,
-    time,
+    time: time || undefined,
     tag,
+    groupTag, // Salva a Tagzona
     completed: false,
-    date: getTodayDate(), // Data correta no formato local "YYYY-MM-DD"
+    date: getTodayDate(),
     duration,
   };
 }
