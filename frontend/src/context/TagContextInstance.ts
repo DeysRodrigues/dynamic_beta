25e098b4ba4d0da89b8ebb626a08f157c2918625
Deys <deysprogrammer@gmail.com>
2025-11-28 20:13:56 -0300

    refactor: removed legacy Context API files and providers

--- frontend/src/context/TagContextInstance.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-type TagContextType = {
-  tags: string[];
-  addTag: (tag: string) => void;
-  removeTag: (index: number) => void;
-};
-
-export const TagContext = createContext<TagContextType | undefined>(undefined);
