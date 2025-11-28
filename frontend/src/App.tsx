e609eb5e9a2798fa66a9e9e41d7fa522234ff54a
Deys <deysprogrammer@gmail.com>
2025-11-28 20:23:36 -0300

    refactor: cleaned up App structure and added Planner route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,22 @@
-"use client";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardLayout from "./components/layout/DashboardLayout";
 import Home from "./pages/HomePage";
 import TasksPage from "./pages/TasksPage";
-import GraphicsPage from "./pages/GraphicsPage";
-import { TaskProvider } from "./context/TaskContext";
-import { DashboardProvider } from "./context/DashboardContext";
-import { GoalsProvider } from "./context/GoalsContext";
-import { TagProvider } from "./context/TagContext";
+import WeekPlannerPage from "./pages/WeekPlannerPage";
 
 
+// Observe: Sem Providers aqui! O Zustand gerencia o estado globalmente.
 export default function App() {
   return (
-    <TagProvider>
-      <GoalsProvider>
-        <TaskProvider>
-          <DashboardProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route element={<DashboardLayout />}>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/tasks" element={<TasksPage />} />
-                  {/* <Route path="/timedtasks" element={<TimedTasksPage/>} />
-                  <Route path="/settings" element={<Settings />} /> */}
-                  <Route path="/graphics" element={<GraphicsPage />} />
-                </Route>
-              </Routes>
-            </BrowserRouter>
-          </DashboardProvider>
-        </TaskProvider>
-      </GoalsProvider>
-    </TagProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DashboardLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/planner" element={<WeekPlannerPage />} /> {/* <--- Nova Rota */}
+ 
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
